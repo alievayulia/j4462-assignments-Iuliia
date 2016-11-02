e84d4cd84ec667fad81b5976f3e625f79bebcdb9
Iuliia <yaad5d@mail.missouri.edu>
2016-11-02 01:51:58 -0500

    Got this error

--- scraper_Iuliia.py
+++ scraper_Iuliia.py
@@ -2,6 +2,10 @@ import urllib2, csv
 import mechanize
 from bs4 import BeautifulSoup
 
+# Open file and CSV writer
+outfile = open('election.csv', 'w')
+writer = csv.writer(outfile)
+
 br = mechanize.Browser()
 br.open('http://enrarchives.sos.mo.gov/enrnet/PickaRace.aspx')
 
@@ -18,4 +22,35 @@ br.submit('ctl00$MainContent$btnCountyChange')
 # Get HTML
 html = br.response().read()
 
-########## YOUR CODE HERE ##########
+# Set up BeautifulSoup
+soup = BeautifulSoup(html, "html.parser")
+
+# Find table body/rows
+table = soup.find('table', {'class': 'electtable'})
+rows = table.find_all('tr')
+
+for row in rows:
+
+
+  # Here's one way to get all of the data into a list
+
+    # # Get all of the cells (td tags) for each row
+    # cells = row.find_all('td')
+
+    # # Create a new empty list to hold the output
+    # data = []
+
+    # # Loop over each cell in the row
+    # for cell in cells:
+    #     # Add the cell's data to the row_data output
+    #     data.append(cell.text)
+
+    # Here's another way to do it with list comprehensions
+    data = [cell.text for cell in row.find_all('td')]
+
+    writer.writerow(data)
+
+    # # Traceback (most recent call last):
+  # File "scraper_Iuliia.py", line 51, in <module>
+    # writer.writerow(data)
+# UnicodeEncodeError: 'ascii' codec can't encode character u'\xa0' in position 0: ordinal not in range(128)
